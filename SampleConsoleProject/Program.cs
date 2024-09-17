9c75c958be15226f8a6f794cc9fd0f028e77961f
tgibbons-css <tgibbons@css.edu>
2024-09-17 10:59:41 -0500

    Updated assignment

--- SampleConsoleProject/Program.cs
+++ SampleConsoleProject/Program.cs
@@ -4,3 +4,5 @@ Console.WriteLine("Adding a line in Visual Studio");
 // added this line -- added to comment
 Console.WriteLine("Adding a line in VS Code");
 
+// Tom added this new comment from his assignment
+
